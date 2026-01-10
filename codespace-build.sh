a32942e67dbafc4bff98c162b012dfa31538a35b
mikenrafter <88250914+mikenrafter@users.noreply.github.com>
2026-01-10 08:34:15 +0000

    Minor tweaks

--- codespace-build.sh
+++ codespace-build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
-export PATH="$JAVA_HOME/bin:$PATH"
+export ANDROID_HOME=/opt/android-sdk
+export PATH=$JAVA_HOME/bin:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools:$PATH
 java -version
-exec ./gradlew clean :SaidIt:assembleDebug --no-daemon
 sudo apt install openjdk-17-jdk -y
+exec ./gradlew clean :SaidIt:assembleDebug --no-daemon
